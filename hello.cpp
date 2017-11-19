58899e2a01b6716874bbe19cd5abac4c97471cac
Jacob Killelea <jkillelea@protonmail.ch>
2017-11-18 21:59:04 -0800

    style changes

--- hello.cpp
+++ hello.cpp
@@ -5,13 +5,11 @@
 #include <vector>
 #include <string>
 
-
 #include "util.hpp"
 cl_int err;
 
 using std::string;
 using std::vector;
-
 
 int main(int argc, char const *argv[]) {
   vector<cl::Platform> platforms;
@@ -27,31 +25,31 @@ int main(int argc, char const *argv[]) {
   string src = read_to_string("helloworld.cl");
 
   // typedef vector<std::pair<const char*, size_type> > Sources;
-  std::cout << "[sources]" << '\n';
+  std::cout << "[sources]\n";
   cl::Program::Sources sources(1, src);
 
-  std::cout << "[context]" << '\n';
+  std::cout << "[context]\n";
   cl::Context context(device);
 
-  std::cout << "[program]" << '\n';
+  std::cout << "[program]\n";
   cl::Program program(context, src);
   check_error();
 
 
-  std::cout << "[program.build]" << '\n';
+  std::cout << "[program.build]\n";
   err = program.build(CL_BUILD_VERSION_TARGET);
   // print_build_info(program, true);
 
   char buf[16];
   cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
 
-  std::cout << "[kernel]" << '\n';
+  std::cout << "[kernel]\n";
   cl::Kernel kernel(program, "helloworld", &err);
   check_error();
 
   kernel.setArg(0, memBuf);
 
-  std::cout << "[command queue]" << '\n';
+  std::cout << "[command queue]\n";
   cl::CommandQueue queue(context);
 
   // global range of 1 (1 instance only)
